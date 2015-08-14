39cb831149f1e2331c6d24acbf927021809f99cd
alexnicolini <alexnicolini@gmail.com>
2015-08-14 17:59:20 -0300

    Adicionados artigos suficientes para que a paginação possa ser usada

--- server/fixtures.js
+++ server/fixtures.js
@@ -39,4 +39,15 @@ if (Posts.find().count() === 0) {
     submitted: new Date(now - 12 * 3600 * 1000),
     commentsCount: 0
   });
+
+  for (var i = 0; i < 10; i++) {
+    Posts.insert({
+      title: 'The post #' + i,
+      author: sacha.profile.name,
+      userId: sacha._id,
+      url: 'http://google.com/?q=test-' + i,
+      submitted: new Date(now - i * 3600 * 1000 + 1),
+      commentsCount: 0
+    });
+  }
 }
