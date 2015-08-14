0ee23df3f96dcf1d2104bfb9c799221023b36052
alexnicolini <alexnicolini@gmail.com>
2015-08-14 13:21:35 -0300

    Exibir as notificações no header

--- collections/notifications.js
+++ collections/notifications.js
@@ -2,14 +2,15 @@ Notifications = new Mongo.Collection('notifications');
 
 Notifications.allow({
   update: function (userId, doc, fieldNames) {
-    return ownsDocument(userId, doc) &&
-      fieldNames.lenght === 1 && fieldNames[0] === 'read';
+    // return ownsDocument(userId, doc) &&
+    //   fieldNames.lenght === 1 && fieldNames[0] === 'read';
+    return ownsDocument(userId, doc);
   }
 });
 
 createCommentNotification = function(comment) {
   var post = Posts.findOne(comment.postId);
-  if (comment.userId !== postId) {
+  if (comment.userId !== post.userId) {
     Notifications.insert({
       userId: post.userId,
       postId: post._id,
